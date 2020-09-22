5821e075347cc7a80067580dbce889a73098694d
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2020-09-22 11:28:09 +0200

    Create methods with POST error

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,14 @@ public class HomeController {
         List<Product> productList = Prodservice.fetchAll();
         model.addAttribute("products", productList);
         return "index";
+    }
+
+    @GetMapping("/create") String createProduct(){ return "/productCreate"; }
+
+    @PostMapping("/create")
+    String createProduct(@ModelAttribute Product product){
+        Prodservice.add(product);
+        return "redirect:/index";
     }
     @GetMapping("/delete/{product_id}")
     public String delete(@PathVariable("product_id") int id){
