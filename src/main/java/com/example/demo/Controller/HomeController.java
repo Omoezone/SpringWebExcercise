60722791898d7da9af74dcc82f73f3a386b13969
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-10-30 10:47:45 +0100

    sad

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -16,41 +16,5 @@ import java.util.List;
 
 @Controller
 public class HomeController {
-    @Autowired
-    ProductService Prodservice;
-
-    @GetMapping("/")
-    String home(Model model){
-        List<Product> productList = Prodservice.fetchAll();
-        model.addAttribute("products", productList);
-        return "index";
-    }
-
-    @GetMapping("/create") String productCreate(){ return "productCreate"; }
-
-    @PostMapping("/productCreate")
-    String productCreate(@ModelAttribute Product product){
-        Prodservice.add(product);
-        return "redirect:/";
-    }
-    @GetMapping("/delete/{product_id}")
-    public String delete(@PathVariable("product_id") int id){
-        boolean deleted = Prodservice.delete(id);
-        if(deleted){
-            return "redirect:/";
-        }else {
-            return "redirect:/";
-        }
-    }
-    @GetMapping("/productUpdate/{product_id}")
-    public String productUpdate(@PathVariable("product_id") int product_id, Model model){
-        model.addAttribute("product", Prodservice.findById(product_id));
-        return "productUpdate";
-    }
-    @PostMapping("/productUpdate")
-    public String productUpdate(@ModelAttribute Product product){
-        Prodservice.update(product);
-        return "redirect:/";
-    }
 
 }
--- src/main/java/com/example/demo/Repository/ProdRepo.java
+++ src/main/java/com/example/demo/Repository/ProdRepo.java
@@ -1,4 +1,15 @@
 package com.example.demo.Repository;
 
-public interface ProdRepo {
+import com.example.demo.Model.Product;
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+import java.util.Set;
+
+
+public interface ProdRepo extends CrudRepository<Product, Long> {
+
+   Optional<Product> findById(int id);
+   Set<Product> findAll();
+   // Set<Product> findAllByXxx(String x);
+
 }
