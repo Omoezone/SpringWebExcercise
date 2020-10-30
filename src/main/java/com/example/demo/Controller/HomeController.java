ce7b8a75afa5e9c36ce2c6c74187c2be15098571
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-10-30 10:55:20 +0100

    sad

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,20 +1,14 @@
 package com.example.demo.Controller;
 
-import com.example.demo.Model.Product;
-import com.example.demo.Service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.Repository.ProdRepo;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class HomeController {
-
+    ProdRepo prodRepo;
+    // constructor injection
+    public HomeController(ProdRepo recipeRepository ){
+        this.prodRepo = prodRepo;
+    }
 }
