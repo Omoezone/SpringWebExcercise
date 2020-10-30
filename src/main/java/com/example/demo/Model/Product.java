693bc061b6129b1aa9eef475076d41ec2953a1c1
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2020-10-30 10:34:02 +0100

    Generated id value

--- src/main/java/com/example/demo/Model/Product.java
+++ src/main/java/com/example/demo/Model/Product.java
@@ -2,11 +2,14 @@ package com.example.demo.Model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int product_id;
     private String product_name;
     private String product_desc;
