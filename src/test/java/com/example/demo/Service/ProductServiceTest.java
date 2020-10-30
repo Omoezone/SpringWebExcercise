d1d2f1c86b8f57875a0e5d6e81001e699b5a212f
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2020-10-30 10:24:11 +0100

    Bootstrap styling in productCreate.html + home button WiP in same

---
+++ src/test/java/com/example/demo/Service/ProductServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.Service;
+
+import com.example.demo.Model.Product;
+import com.example.demo.Repository.ProductRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+    @InjectMocks
+    ProductService mockProductService;
+
+    @Mock
+    ProductRepo mockProductRepo;
+
+    @BeforeEach
+    void setUp() {
+        // Instantiate mock repository
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getProductPrice(){
+        Product product = new Product(5, "Gummiged", "Stor ged lavet af gummi", 100.0);
+//        Mockito.when();
+    }
+}
