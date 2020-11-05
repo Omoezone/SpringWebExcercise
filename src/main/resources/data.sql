6154dea0310c3a8f4b3182d04a7b579e28382881
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-11-05 10:38:48 +0100

    sad

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,14 +21,10 @@ INSERT INTO note (desc, product_product_id)
 INSERT INTO note (desc, product_product_id)
 VALUES ('This was made to fuck with poor people', 2);
 
-INSERT INTO company (id, name)
-    VALUES (1, 'Bandidos');
-INSERT INTO company (id,name)
-    VALUES (2, 'Hells Angels');
-INSERT INTO company (id, name)
-    VALUES (3, 'KEA');
-
-UPDATE company SET product_id = 4 WHERE id = 1;
-UPDATE company SET product_id = 2 WHERE id = 2;
-UPDATE company SET product_id = 1 WHERE id = 3;
+INSERT INTO company (name,product_product_id)
+    VALUES ('Bandidos',1);
+INSERT INTO company (name,product_product_id)
+    VALUES ('Hells Angels',2);
+INSERT INTO company (name,product_product_id)
+    VALUES ('KEA',3);
 
