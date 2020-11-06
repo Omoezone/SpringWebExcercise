3acf34472c9e8cca3e9c0cc484096c7bf0fafe06
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-11-06 10:02:36 +0100

    sad

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,22 +1,33 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.prodService;
 import com.example.demo.Repository.ProdRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class HomeController {
 
     @Autowired
-    ProdRepo repo;
+    prodService service;
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("products", repo.findAll());
+        model.addAttribute("products", service.findAll());
 
         return "index";
     }
+    @GetMapping("/")
+    public String createProduct(){
+        return "something";
+    }
+    @PostMapping("/")
+    public String delete(long id){
+
+    }
+
 }
---
+++ src/main/java/com/example/demo/Model/prodService.java
@@ -0,0 +1,45 @@
+package com.example.demo.Model;
+
+import com.example.demo.Repository.ProdRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class prodService {
+    @Autowired
+    ProdRepo prodRepo;
+
+    // Denne findbyId er optional
+    public Product findbyId(long id){
+        Optional<Product> opProd = prodRepo.findById(id);
+        // hvis ingen product der matcher det søgte id, vil der blive kastet en fejl
+        if(!opProd.isPresent()){
+            throw new RuntimeException("No product with matching id");
+        }
+
+        return opProd.get();
+    }
+
+    public Set<Product> findAll()
+    {
+        //kald repo.findAll og returner
+        Set<Product> products = new HashSet<>();
+        for(Product product:prodRepo.findAll()) {
+            products.add(product);
+        }
+        return products;
+    }
+    public void create(Product product)
+    {
+        prodRepo.save(product);
+    }
+
+    // ved update skal id være sat
+    public void update(Product product) { prodRepo.save(product); }
+
+    public void delete(Long id) { prodRepo.deleteById(id); }
+}
