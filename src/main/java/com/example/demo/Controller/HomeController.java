d75511740a029e790a674fe2099534e358b8c34c
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-11-06 08:54:25 +0100

    sad

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Repository.ProdRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
-    ProdRepo prodRepo;
-    // constructor injection
-    public HomeController(ProdRepo recipeRepository ){
-        this.prodRepo = prodRepo;
+
+    @Autowired
+    ProdRepo repo;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("products", repo.findAll());
+
+        return "index";
     }
 }
