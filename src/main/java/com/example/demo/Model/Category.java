7c90d0456a881af116a795b4034ce99303b8904f
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2020-11-11 14:48:01 +0100

    Product update html and homecontroller set up

--- src/main/java/com/example/demo/Model/Category.java
+++ src/main/java/com/example/demo/Model/Category.java
@@ -15,4 +15,28 @@ public class Category {
 
     @OneToMany(mappedBy = "category")
     Set<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
